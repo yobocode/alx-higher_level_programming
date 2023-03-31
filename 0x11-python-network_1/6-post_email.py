fb409d80ac3fd7bd36df8cc559fae1377ee636b2
yobocode <yibrahamare@gmail.com>
2023-03-31 02:31:56 -0700

    task 5

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
-"""
+""" post email """
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
     print(r.text)
